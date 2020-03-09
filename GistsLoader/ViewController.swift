14449999d6ee1c3d82f5adac19a110633dda5a56
anthropomorphous <lampochka99@bk.ru>
2020-03-09 15:36:13 +0300

    Initial Commit

---
+++ GistsLoader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GistsLoader
+//
+//  Created by ios_school on 3/9/20.
+//  Copyright © 2020 ios_school. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
